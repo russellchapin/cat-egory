66d1ba82fcbb51f977bc9a9557df30b83d452171
Russell Chapin <russell_chapin@apple.com>
2019-05-05 21:41:42 -0700

    Updates

---
+++ precache-manifest.8c72bb6afc4cd72e52a4cc8402a0c430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812a1e0cd6a61bae91fd55b7fe3fe5fa",
+    "url": "/cat-egory/index.html"
+  },
+  {
+    "revision": "3153f31c619d81ca6840",
+    "url": "/cat-egory/static/css/main.ce0914a3.chunk.css"
+  },
+  {
+    "revision": "950bfcc3792b7ac9b939",
+    "url": "/cat-egory/static/js/2.c407f00e.chunk.js"
+  },
+  {
+    "revision": "3153f31c619d81ca6840",
+    "url": "/cat-egory/static/js/main.764564d9.chunk.js"
+  },
+  {
+    "revision": "378f06187431084d05a2",
+    "url": "/cat-egory/static/js/runtime~main.5e3d575d.js"
+  }
+]);
